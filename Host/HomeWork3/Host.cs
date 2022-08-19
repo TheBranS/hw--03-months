90219fdf5e5d7ee804647f34b7108e1a01bf72f7
TheBranS <110929521+TheBranS@users.noreply.github.com>
2022-08-19 16:23:23 +0300

    Proba pera
    Надеюсь получилось

---
+++ Host/HomeWork3/Host.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace HomeWork3
+{
+    class Host
+    {
+        public static int Main()
+        {               
+            if (Lang.Months.NumOfMonth() == 12 || Lang.Months.NumOfMonth() == 1 || Lang.Months.NumOfMonth() == 2)
+            {
+                Console.WriteLine("Сезон года - Зима");
+            }
+            else if (Lang.Months.NumOfMonth() == 3 || Lang.Months.NumOfMonth() == 4 || Lang.Months.NumOfMonth() == 5)
+            {
+                Console.WriteLine("Сезон года - Весна");
+            }
+            else if (Lang.Months.NumOfMonth() == 6 || Lang.Months.NumOfMonth() == 7 || Lang.Months.NumOfMonth() == 8)
+            {
+                Console.WriteLine("Сезон года - Лето");
+            }
+            else if (Lang.Months.NumOfMonth() == 9 || Lang.Months.NumOfMonth() == 10 || Lang.Months.NumOfMonth() == 11)
+            {
+                Console.WriteLine("Сезон года - Осень");
+            }
+            return 0;
+        }
+    }
+}
---
+++ Host/Lang/Months.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Lang 
+{
+    public class Months
+    {
+        public static int NumOfMonth()
+        {
+            Console.WriteLine("Выберите номер месяца от 1 до 12");
+            int selection = Convert.ToInt16(Console.ReadLine());
+
+            switch (selection)
+            {
+                case 1:
+                    Console.WriteLine("Вы выбрали 1ый месяц - Январь, в нём 31 дней");
+                    break;
+                case 2:
+                    Console.WriteLine("Вы выбрали 2ой месяц - Февраль, в нём 28 дней");
+                    break;
+                case 3:
+                    Console.WriteLine("Вы выбрали 3ий месяц - Март, в нём 31 дней");
+                    break;
+                case 4:
+                    Console.WriteLine("Вы выбрали 4ый месяц - Апрель, в нём 30 дней");
+                    break;
+                case 5:
+                    Console.WriteLine("Вы выбрали 5ый месяц - Май, в нем 31 дней");
+                    break;
+                case 6:
+                    Console.WriteLine("Вы выбрали 6ой месяц - Июнь, в нем 30 дней");
+                    break;
+                case 7:
+                    Console.WriteLine("Вы выбрали 7ой месяц - Июль, в нем 31 дней");
+                    break;
+                case 8:
+                    Console.WriteLine("Вы выбрали 8ой месяц - Август,в нем 31 дней ");
+                    break;
+                case 9:
+                    Console.WriteLine("Вы выбрали 9ый месяц - Сентябрь, в нем 30 дней");
+                    break;
+                case 10:
+                    Console.WriteLine("Вы выбрали 10ый месяц - Октябрь, в нем 31 дней ");
+                    break;
+                case 11:
+                    Console.WriteLine("Вы выбрали 11ый месяц - Ноябрь, в нем 30 дней");
+                    break;
+                case 12:
+                    Console.WriteLine("Вы выбрали 12ый месяц - Декабрь, в нем 31 дней ");
+                    break;
+                default:
+                    Console.WriteLine("Вы выбрали несуществующий месяц");
+                    break;
+            }
+            return selection;
+        }
+    }
+}
